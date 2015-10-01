0cdd7260a533a7378d7fd16e734842a50a915cd7
Kevin Lin <kevinlin505@hotmail.com>
2015-10-01 17:06:40 -0400

    Pets done

---
+++ objc-PetCast/FISDog.h
@@ -0,0 +1,16 @@
+//
+//  FISDog.h
+//  objc-PetCast
+//
+//  Created by Kevin Lin on 10/1/15.
+//  Copyright © 2015 The Flatiron School. All rights reserved.
+//
+
+#import "FISPet.h"
+
+@interface FISDog : FISPet
+
+-(NSString *)makeASound;
+-(NSString *)assaultTheMailman;
+
+@end
---
+++ objc-PetCast/FISPet.h
@@ -0,0 +1,16 @@
+//
+//  FISPet.h
+//  objc-PetCast
+//
+//  Created by Kevin Lin on 10/1/15.
+//  Copyright © 2015 The Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISPet : NSObject
+
+-(NSString *)makeASound;
+-(NSString *)eatSomething;
+
+@end
