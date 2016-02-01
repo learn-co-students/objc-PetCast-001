e0b8f4485c930b51ed97b792c459477964f7962e
kenmhaggerty <kenmhaggerty@gmail.com>
2016-02-01 11:30:33 -0500

    Completes exercise

--- objc-PetCast/FISAppDelegate.m
+++ objc-PetCast/FISAppDelegate.m
@@ -1,10 +1,31 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISDog.h"
 
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FISDog *dog = [[FISDog alloc] init];
+    NSLog(@"%@", [dog makeASound]);
+    NSLog(@"%@", [dog assaultTheMailman]);
+    
+    FISPet *dogAsPet = (FISPet *)dog;
+    NSLog(@"%@", [dogAsPet makeASound]);
+    
+    FISPet *pet = [[FISPet alloc] init];
+    FISDog *petAsDog = (FISDog *)pet;
+    NSLog(@"%@", [petAsDog makeASound]);
+    if ([petAsDog respondsToSelector:@selector(assaultTheMailman)]) {
+        NSLog(@"%@", [petAsDog assaultTheMailman]);
+    }
+    else {
+        NSLog(@"petAsDog cannot assaultTheMailman");
+    }
+    
+    NSLog(@"%@", [((FISDog *)dogAsPet) assaultTheMailman]);
+    
     return YES;
 }
 
---
+++ objc-PetCast/FISDog.m
@@ -0,0 +1,23 @@
+//
+//  FISDog.m
+//  objc-PetCast
+//
+//  Created by Ken M. Haggerty on 2/1/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import "FISDog.h"
+
+@implementation FISDog
+
+- (NSString *)makeASound {
+    
+    return @"Woof!";
+}
+
+- (NSString *)assaultTheMailman {
+    
+    return @"I got the mail! I got the mail!";
+}
+
+@end
