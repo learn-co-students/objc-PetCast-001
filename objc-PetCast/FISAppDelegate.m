23f9f9e0ff64e54b1f36520843f4e10047fe1aee
aabato <angelica.bato@gmail.com>
2016-02-07 11:30:16 -0500

    Make FISPet into FISDog and assault the mailman  ┬┴┬┴┤ʕ•ᴥ├┬┴┬┴

--- objc-PetCast/FISAppDelegate.m
+++ objc-PetCast/FISAppDelegate.m
@@ -1,10 +1,25 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISDog.h"
 
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FISDog *bo = [[FISDog alloc] init];
+    NSLog(@"%@",[bo assaultTheMailman]);
+    
+    NSLog(@"%@",[((FISPet *)bo) makeASound]);
+    
+    FISPet *wesley = [[FISDog alloc] init];
+    NSLog(@"%@",[((FISPet *)wesley) makeASound]); //Cannot make it say "Pet me!" because it requires to initialize it as an FISPet rather than FISdog --- casting does not change this.
+    
+    NSLog(@"%@",[((FISDog *) wesley) assaultTheMailman]);
+    
+    
+    
+    
     return YES;
 }
 
---
+++ objc-PetCast/FISPet.m
@@ -0,0 +1,21 @@
+//
+//  FISPet.m
+//  objc-PetCast
+//
+//  Created by Angelica Bato on 2/7/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import "FISPet.h"
+
+@implementation FISPet
+
+-(NSString *)makeASound {
+    return @"Pet me!";
+}
+
+-(NSString *)eatSomething {
+    return @"Nom nom nom.";
+}
+
+@end
