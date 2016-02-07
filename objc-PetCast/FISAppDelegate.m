bfcad6d3ec2140339b73868bb2198f53304e43d9
Julia <julia@q.local>
2016-02-07 03:19:08 -0500

    Class casting

--- objc-PetCast/FISAppDelegate.m
+++ objc-PetCast/FISAppDelegate.m
@@ -1,10 +1,21 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISDog.h"
+
 
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    FISDog *osvaldo = [[FISDog alloc]init];
+    FISDog *henry = [[FISDog alloc]init];
+    [osvaldo assaultTheMailman];
+    [henry assaultTheMailman];
+    NSLog(@"%@", [(FISPet *)osvaldo makeASound]);
+    NSLog(@"%@", [henry makeASound]);
+    FISPet *enrique = [[FISDog alloc]init];
+    NSLog(@"%@", [(FISDog *)enrique makeASound]);
+    NSLog(@"%@", [((FISDog *)enrique) assaultTheMailman]);
     return YES;
 }
 
