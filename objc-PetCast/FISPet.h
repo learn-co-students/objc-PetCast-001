6c133fa5cf044190a4e7215914726d04a328737d
SamuelBoyce <samuel.boyce@gmail.com>
2016-02-08 15:15:54 -0500

    [FISDeathstar isAMoon]

---
+++ objc-PetCast/FISPet.h
@@ -0,0 +1,19 @@
+//
+//  FISPet.h
+//  objc-PetCast
+//
+//  Created by Samuel Boyce on 2/8/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISPet : NSObject
+
+
+-(NSString *)makeASound;
+
+-(NSString *)eatSomething;
+
+
+@end
---
+++ objc-PetCast/FISPet.m
@@ -0,0 +1,25 @@
+//
+//  FISPet.m
+//  objc-PetCast
+//
+//  Created by Samuel Boyce on 2/8/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import "FISPet.h"
+
+@implementation FISPet
+
+
+-(NSString *)makeASound
+{
+    return @"Pet me!";
+}
+
+-(NSString *)eatSomething
+{
+    return @"Nom nom nom.";
+}
+
+
+@end
