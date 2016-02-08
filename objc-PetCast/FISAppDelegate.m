26bfa03aa1a7af641b78986996b82a1417e5f26e
andynovak12 <andynovak12@gmail.com>
2016-02-08 13:35:01 -0500

    dogs are cast

--- objc-PetCast/FISAppDelegate.m
+++ objc-PetCast/FISAppDelegate.m
@@ -1,11 +1,30 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISDog.h"
 
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FISDog *doggie = [[FISDog alloc]init];
+    NSLog (@"%@", [doggie makeASound]);
+    NSLog(@"%@", [doggie assaultTheMailman]);
+    
+    FISPet *doggieCastAsAPet = (FISPet *)doggie;
+    NSLog (@"%@", [doggieCastAsAPet makeASound]);
+
+    FISPet *iAmAPet = [[FISPet alloc]init];
+    NSLog (@"%@", [iAmAPet makeASound]);
+    FISDog *iAmAPetButCastAsADog = (FISDog *)iAmAPet;
+    NSLog (@"%@", [iAmAPetButCastAsADog makeASound]);
+ //   NSLog(@"%@", [iAmAPetButCastAsADog assaultTheMailman]);
+    
+    NSLog(@"%@", [((FISDog *)doggieCastAsAPet) assaultTheMailman]);
+
+
     return YES;
 }
 
 @end
+
