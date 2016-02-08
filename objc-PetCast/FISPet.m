55a9c0f9e69b3afa5f4d11e9b45054cbcdac88ee
sguevara11 <stephanie.n.guevara@gmail.com>
2016-02-08 15:24:36 -0500

    Played with casting

---
+++ objc-PetCast/FISPet.m
@@ -0,0 +1,24 @@
+//
+//  FISPet.m
+//  objc-PetCast
+//
+//  Created by Stephanie on 2/8/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import "FISPet.h"
+
+@implementation FISPet
+
+-(NSString *)makeASound{
+    
+    return @"Pet me!";
+}
+
+-(NSString *)eatSomething{
+    
+    return @"Nom nom nom.";
+    
+}
+
+@end
