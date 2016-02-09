499ba4d314ba4136ef5560a7e6cd10eae45b45d0
SunFloweri <irinakalashnikova@gmail.com>
2016-02-09 16:45:38 -0500

    Add class FISPet and subclass FISDog with methods

--- objc-PetCast/FISAppDelegate.m
+++ objc-PetCast/FISAppDelegate.m
@@ -1,11 +1,37 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISPet.h"
+#import "FISDog.h"
 
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    FISDog *dog = [[FISDog alloc] init];
+    FISPet *dogPet = (FISPet *)dog;
+    
+    FISPet *pet = [[FISPet alloc] init];
+    FISDog *petDog = (FISDog *)pet;
+    
+    NSLog(@"\n ******* dog: ******** \n");
+    NSLog(@"%@", dog.makeASound);
+    NSLog(@"%@", dog.assaultTheMailman);
+    NSLog(@"%@", dog.eatSomething);
+    
+    NSLog(@"\n ******* dogPet: ******** \n");
+    NSLog(@"%@", dogPet.makeASound);
+    //NSLog(@"%@", dogPet.assaultTheMailman);
+    NSLog(@"%@", dogPet.eatSomething);
+    
+    NSLog(@"\n ******* pet: ******** \n");
+    NSLog(@"%@", pet.makeASound);
+    NSLog(@"%@", pet.eatSomething);
+    
+    NSLog(@"\n ******* petDog: ******** \n");
+    NSLog(@"%@", petDog.makeASound);
+    //NSLog(@"%@", petDog.assaultTheMailman);
+    
+    
     return YES;
 }
-
 @end
