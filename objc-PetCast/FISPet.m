fad1ecfda9a20044df5bfd45a78bb959df534300
ArielSD <ascottdicker@dosomething.org>
2016-02-09 13:43:40 -0500

    Done

---
+++ objc-PetCast/FISPet.m
@@ -0,0 +1,23 @@
+//
+//  FISPet.m
+//  objc-PetCast
+//
+//  Created by Ariel Scott-Dicker on 2/9/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import "FISPet.h"
+
+@implementation FISPet
+
+- (NSString *)makeASound {
+    NSString *petMe = @"Pet me!";
+    return petMe;
+}
+
+- (NSString *)eatSomething {
+    NSString *nomNomNom = @"Nom nom nom.";
+    return nomNomNom;
+}
+
+@end
