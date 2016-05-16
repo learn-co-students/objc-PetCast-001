ea89631bd77505961e0eb2a80722bed4e2c7e790
John Richardson <richardsonjm@gmail.com>
2016-05-16 11:39:53 -0400

    Casting practice

--- objc-PetCast/FISAppDelegate.m
+++ objc-PetCast/FISAppDelegate.m
@@ -5,6 +5,20 @@
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+
+    FISDog *fido = [[FISDog alloc] init];
+    NSLog(@"%@", fido.makeASound);
+    NSLog(@"%@", fido.assaultTheMailman);
+
+    FISPet *dogAsPet = (FISPet *)fido;
+    NSLog(@"%@", dogAsPet.makeASound);
+
+    FISPet *pet = [[FISPet alloc] init];
+    FISDog *petAsDog = (FISDog *)pet;
+    NSLog(@"%@", petAsDog.makeASound);
+//    NSLog(@"%@", petAsDog.assaultTheMailman);
+    NSLog(@"%@", ((FISDog *)dogAsPet).assaultTheMailman);
+
     return YES;
 }
 
