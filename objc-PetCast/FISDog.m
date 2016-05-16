eb3c94397934adc69c990f91f9301019362437a2
John Richardson <richardsonjm@gmail.com>
2016-05-16 11:20:27 -0400

    Build pet and dog classes

---
+++ objc-PetCast/FISDog.m
@@ -0,0 +1,21 @@
+//
+//  FISDog.m
+//  objc-PetCast
+//
+//  Created by John Richardson on 5/16/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import "FISDog.h"
+
+@implementation FISDog
+
+- (NSString *)makeASound {
+    return @"Woof!";
+}
+
+- (NSString *)assaultTheMailman {
+    return @"I got the mail! I got the mail!";
+}
+
+@end
